ec0a013a29d896c7c62e7df0c32c0c128b7d1d1b
adityapatel02 <154357668+adityapatel02@users.noreply.github.com>
2024-10-30 09:13:42 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,52 +1,16 @@
-const User = require("../models/user.js");
-const wrapAsync = require("../utils/wrapAsync");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-//Signup page
-let signup =  (req, res) => {
-    res.render("users/signup");
-}
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required: true,
+    },
+});
 
-//Signup
-let signupPost = wrapAsync(async (req, res) => {
-    try {let data = req.body;
-         let user = new User(data);
-         let registeredUser = await User.register(user, data.password);
-         console.log(registeredUser);
-         req.login(registeredUser,(err)=>{
-             if(err){
-               return next(err);
-             }
-             req.flash("success", "Registered Successfully!");
-             res.redirect("/listings");
-         })   
-    } catch (error) {
-            req.flash("error", error.message);
-            res.redirect("/signUp");
-    }  
-  })
+userSchema.plugin(passportLocalMongoose);
 
+const User= mongoose.model('User', userSchema);
 
-//login 
-let login =  async (req, res) => {
-    req.flash("success", "Logged in Successfully!");
-    res.redirect( res.locals.redirectUrl || "/listings");
-  }
-
-
-//logout
-let logout = (req,res)=>{
-    req.logout((err)=>{
-        if (err) {
-            next(err);
-        }
-   req.flash("success","You are logged out!");
-   res.redirect("/listings");
-    })
- }
-
-module.exports = {
-    signup:signup,
-    signupPost:signupPost,
-    login:login,
-    logout:logout,
-}
+module.exports = User;
